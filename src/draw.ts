7bb9ed1d3ef318bdfe28abde5b46483e8b0bc152
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-10-25 22:34:28 -0300

    separando em mais funções para maior flexibilidade

--- src/draw.ts
+++ src/draw.ts
@@ -4,16 +4,23 @@ export type Vector2 = {
     y: number
 };
 
-export function drawPolygon(ctx: CanvasRenderingContext2D, position: Vector2, polygon: readonly Vector2[]) {
+export function makePolygonWithAbsolutePosition(position: Vector2, polygon: readonly Vector2[]): Vector2[] {
+    return polygon.map(point => ({
+        x: position.x + point.x,
+        y: position.y + point.y
+    }));
+}
+
+export function drawPolygon(ctx: CanvasRenderingContext2D, polygon: readonly Vector2[]) {
     let point: Vector2 = polygon[0];
     ctx.beginPath();
-    ctx.moveTo(position.x + point.x, position.y + point.y);
+    ctx.moveTo(point.x, point.y);
     for (let i = 1; i < polygon.length; i++) {
         point = polygon[i];
-        ctx.lineTo(position.x + point.x, position.y + point.y);
+        ctx.lineTo(point.x, point.y);
     }
     point = polygon[0];
-    ctx.lineTo(position.x + point.x, position.y + point.y);
+    ctx.lineTo(point.x, point.y);
     ctx.closePath();
     ctx.lineWidth = 1;
     ctx.strokeStyle = '#FFFFFF';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { drawPolygon, Vector2 } from "./draw.js";
+import { drawPolygon, makePolygonWithAbsolutePosition, Vector2 } from "./draw.js";
 
 const canvas = document.createElement('canvas');
 
@@ -14,6 +14,7 @@ if (ctx === null) throw 'Contexto nulo';
 ctx.fillStyle = '#000';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+// @todo João, definir o formato para trabalhar com os polígonos
 const polygon: Vector2[] = [
     { x: 0, y: 10 },
     { x: 10, y: -10 },
@@ -21,4 +22,6 @@ const polygon: Vector2[] = [
 ];
 const position = { x: 200, y: 200 };
 
-drawPolygon(ctx, position,  polygon);
+drawPolygon(ctx, makePolygonWithAbsolutePosition(position, polygon));
+
+drawPolygon(ctx, makePolygonWithAbsolutePosition({ x: 300, y: 300 }, polygon));
