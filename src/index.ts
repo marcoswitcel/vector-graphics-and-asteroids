0a16b7c218c93f2c59fee7e41b06103c024f4c94
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-10-29 16:58:43 -0300

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,8 @@ import { createCanvas } from "./utils.js";
 
 const canvas = createCanvas(500, 500, document.body);
 const ctx = canvas.getContext('2d');
-const canvas2 = createCanvas(500, 500, document.body);
-const ctx2 = canvas2.getContext('2d');
 
-if (ctx === null || ctx2 === null) throw 'Contexto nulo';
-
-ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+if (ctx === null) throw 'Contexto nulo';
 
 // @todo João, definir o formato para trabalhar com os polígonos
 const polygon: Vector2[] = [
@@ -20,26 +15,13 @@ const polygon: Vector2[] = [
 ];
 const position = { x: 0, y: 0 };
 
-drawPolygon(ctx, makePolygonWithAbsolutePosition(position, polygon));
-
-drawPolygon(ctx, makePolygonWithAbsolutePosition({ x: -0.5, y: 0.75 }, scalePolygon(polygon, 0.1)));
-
-drawPolygon(ctx, makePolygonWithAbsolutePosition({ x: -0.5, y: -0.5 }, scalePolygon(polygon, 0.5)));
-
-drawPolygon(ctx, makePolygonWithAbsolutePosition({ x: 0, y: 0 }, scalePolygon(polygon, 0.25, 0.45)));
-
 const eventLoop = new EventLoop();
-let rotation = 0;
 
 eventLoop.add((time: number) => {
-    ctx2.fillStyle = '#000';
-    ctx2.fillRect(0, 0, canvas.width, canvas.height);
-    position.x += 0.001;
-    if (position.x > 1) {
-        position.x = -1;
-    }
-    rotation += 0.01;
-    drawPolygon(ctx2, makePolygonWithAbsolutePosition(position, rotatePolygon(scalePolygon(polygon, 0.5), rotation)));
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    drawPolygon(ctx, makePolygonWithAbsolutePosition(position, scalePolygon(polygon, 0.5)));
 });
 
 eventLoop.start();
