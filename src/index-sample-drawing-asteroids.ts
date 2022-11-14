eb73daddf9377e3d5834d8b7ce97afe2282f5cce
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-11-13 22:51:08 -0300

    refatorando

--- src/index-sample-drawing-asteroids.ts
+++ src/index-sample-drawing-asteroids.ts
@@ -3,7 +3,7 @@ import { Entity, fragmentationAllowed, hittedMark } from "./entity.js";
 import { EventLoop } from "./event-loop.js";
 import { makeAsteroid } from "./figure.js";
 import { KeyBoardInput } from "./keyboard-input.js";
-import { createCanvas } from "./utils.js";
+import { createCanvas, fragmentAsteroid } from "./utils.js";
 
 // @todo João, analisar e implementar um mecanismo organizado como lidar com
 // cliques no canvas sem quebrar nenhum ordem de execução. Isso para inserir
@@ -41,30 +41,6 @@ let entities = Array(15).fill(0).map(() => {
 });
 
 const eventLoop = new EventLoop();
-
-function fragmentAsteroid(entity: Entity, numberOfFragments = 4): Entity[] {
-    const fragments: Entity[] = [];
-
-    for (let i = 0; i < numberOfFragments; i++) {
-        const scale = entity.scale / numberOfFragments;
-        const fragmentAngle = entity.angle + (i * Math.PI / 2);
-        const radius = rotatePoint({ x: 0, y: entity.scale }, fragmentAngle);
-        const position = {
-            x: entity.position.x + radius.x,
-            y: entity.position.y + radius.y,
-        };
-        const velocity = rotatePoint({ x: entity.velocity.x * 0.85, y: entity.velocity.y * 0.85 }, (i * Math.PI / 8));
-        const hitRadius = entity.hitRadius / numberOfFragments;
-        const fragment = new Entity(position, velocity, { x: 0, y: 0 }, entity.angle, entity.type, hitRadius, scale);
-        if (numberOfFragments / 2 > 1) {
-            fragment.components[fragmentationAllowed] = numberOfFragments / 2;
-        }
-        fragments.push(fragment);
-    }
-
-    return fragments;
-}
-
 
 eventLoop.add((time: number) => {
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { rotatePoint } from "./draw.js";
+import { Entity, fragmentationAllowed } from "./entity.js";
 
 /**
  * Cria um canvas com as dimensões especificadas e adiciona ele a algum elemnto
@@ -18,4 +20,27 @@ export function createCanvas(width: number, height: number, appendTo?: HTMLEleme
     }
 
     return canvas;
+}
+
+export function fragmentAsteroid(entity: Entity, numberOfFragments = 4): Entity[] {
+    const fragments: Entity[] = [];
+
+    for (let i = 0; i < numberOfFragments; i++) {
+        const scale = entity.scale / numberOfFragments;
+        const fragmentAngle = entity.angle + (i * Math.PI / 2);
+        const radius = rotatePoint({ x: 0, y: entity.scale }, fragmentAngle);
+        const position = {
+            x: entity.position.x + radius.x,
+            y: entity.position.y + radius.y,
+        };
+        const velocity = rotatePoint({ x: entity.velocity.x * 0.85, y: entity.velocity.y * 0.85 }, (i * Math.PI / 8));
+        const hitRadius = entity.hitRadius / numberOfFragments;
+        const fragment = new Entity(position, velocity, { x: 0, y: 0 }, entity.angle, entity.type, hitRadius, scale);
+        if (numberOfFragments / 2 > 1) {
+            fragment.components[fragmentationAllowed] = numberOfFragments / 2;
+        }
+        fragments.push(fragment);
+    }
+
+    return fragments;
 }
