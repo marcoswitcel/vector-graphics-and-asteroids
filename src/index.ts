342faf719eae67df9fe96944f685982e84895e05
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-09-11 21:56:49 -0300

    WIP: gamepad

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,10 @@ canvas.addEventListener('dblclick', () => {
  * 'resize' é suficiente para saber a nova resolução da 'window'
  */
 window.addEventListener('resize', () => {
+    // @todo João, se a aplicação estiver pausada o canvas é limpo e fica "transparente",
+    // seria interessante pelo menos colorir o background e escrever pausado novamente.
+    // Porém, por hora só tenho acesso ao EventLoop aqui, e nesse caso, só sei dizer se
+    // o loop está parado. O que geralmente indica 'pausa'
     const newResolution = isFullScreen() ? computeResolution(1) : computeResolution(0.9);
     
     canvas.width = newResolution;
