2d8879f2d9ceec8c926a4ede152483fd65b2b063
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-09-13 22:40:32 -0300

    WIP: gamepad teste script

--- src/index-sample-virtual-gamepad.ts
+++ src/index-sample-virtual-gamepad.ts
@@ -143,20 +143,48 @@ class VirtualGamepad {
         // @todo João, implementar
         throw new Error("Não implementado");
     }
+
+    isKeyPressed(vKey: VirtualKeys) {
+        return this.keyState[vKey];
+    }
 }
 
+const square = document.getElementById('square');
 const gamepad = new VirtualGamepad(document.body);
+
+if (!square) throw "Adicionar o div ao HTML";
 
 gamepad.addGamepadToPage(true);
 
-gamepad.addListener('keydown.a', (event) => {
-    console.log('keydown.a', event);
-    const square = document.querySelector('.square');
-    if (square && square instanceof HTMLElement) {
-        square.style.top = (parseFloat(getComputedStyle(square).top) + 10) + 'px';
+const vKeys: VirtualKeys[] = [ 'a', 'w', 's', 'd', 'space' ];
+for (const vKey of vKeys) {
+    gamepad.addListener(`keydown.${vKey}`, (event) => {
+        console.log(`keydown.${vKey}`, event);
+    });
+    
+    gamepad.addListener(`keyup.${vKey}`, (event) => {
+        console.log(`keyup.${vKey}`, event);
+    });
+};
+
+requestAnimationFrame(function updatePosition(time) {
+
+    if (gamepad.isKeyPressed('a')) {
+        square.style.left = parseInt(getComputedStyle(square).left) - 1 + 'px';
     }
+
+    if (gamepad.isKeyPressed('d')) {
+        square.style.left = parseInt(getComputedStyle(square).left) + 1 + 'px';
+    }
+
+    if (gamepad.isKeyPressed('w')) {
+        square.style.top = parseInt(getComputedStyle(square).top) - 1 + 'px';
+    }
+
+    if (gamepad.isKeyPressed('s')) {
+        square.style.top = parseInt(getComputedStyle(square).top) + 1 + 'px';
+    }
+
+    requestAnimationFrame(updatePosition);
 });
 
-gamepad.addListener('keyup.a', (event) => {
-    console.log('keyup.a', event);
-});
