b89a12009e36689fca3e3627cf233e9b74cece00
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-09-18 23:41:28 -0300

    nova versão...

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,7 +1,8 @@
 import { createMainSimulation } from './main-simulation.js';
 import { createCanvas, computeResolution, isFullScreen, isMobileBrowser } from './utils.js';
 import { VirtualGamepad } from './virtual-gamepad.js';
-const GAME_RESOLUTION = computeResolution(0.9);
+const resolutionScaleNonFullscreen = isMobileBrowser() ? 1 : 0.96;
+const GAME_RESOLUTION = computeResolution(resolutionScaleNonFullscreen);
 const canvas = createCanvas(GAME_RESOLUTION, GAME_RESOLUTION, document.body);
 /**
  * @todo João, acredito que seria interessante explicar na tela que clicando
@@ -37,7 +38,7 @@ window.addEventListener('resize', () => {
     // seria interessante pelo menos colorir o background e escrever pausado novamente.
     // Porém, por hora só tenho acesso ao EventLoop aqui, e nesse caso, só sei dizer se
     // o loop está parado. O que geralmente indica 'pausa'
-    const newResolution = isFullScreen() ? computeResolution(1) : computeResolution(0.9);
+    const newResolution = isFullScreen() ? computeResolution(1) : computeResolution(resolutionScaleNonFullscreen);
     canvas.width = newResolution;
     canvas.height = newResolution;
 });
