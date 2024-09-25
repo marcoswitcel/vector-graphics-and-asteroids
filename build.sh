4c0cc2d081f4054f2a5f1313b3c410904c6f2366
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-09-24 21:34:23 -0300

    script mais legível

--- build.sh
+++ build.sh
@@ -1,8 +1,31 @@
+#!/usr/bin/bash
 
+LINE="========================================="
+
+echo "git checkout gh-pages"
 git checkout gh-pages
+echo $LINE
+
+echo "git pull origin main"
 git pull origin main
+echo $LINE
+
+echo "npm run build"
 npm run build
+echo $LINE
+
+echo "git add public/* -f"
 git add public/* -f
+echo $LINE
+
+echo "git commit -m "nova versão...""
 git commit -m "nova versão..."
+echo $LINE
+
+echo "git push"
 git push
+echo $LINE
+
+echo "git checkout main"
 git checkout main
+echo $LINE
