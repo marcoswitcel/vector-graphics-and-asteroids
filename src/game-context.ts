ed542d36344332f4700a2dbff9be6f6380c6bbd7
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-09-29 15:53:51 -0300

    refatorando

--- src/game-context.ts
+++ src/game-context.ts
@@ -5,19 +5,19 @@ import { makeShipBackwardsFigure, makeShipForwardFigure, makeShipStandingFigure 
  * @todo João, avaliar o que o mais mover para dentro dessa classe
  */
 export class GameContext {
-    isPlayerMoving = false;
-    isPlayerMovingForward = false;
-    asteroidsDestroyedCounter = 0;
-    waveIndex = 0;
-    isPaused = false;
-
     playerAcceleration = { x: 0, y: 0.45 };
     entityPlayer = new Entity({ x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, 0, 'player', 0.07, 0.08);
+    isPlayerMoving = false;
+    isPlayerMovingForward = false;
+    shootWaitingToBeEmmited = false;
+    
     shipStandingFigure = makeShipStandingFigure();
     shipForwardFigure = makeShipForwardFigure();
     shipBackwardsFigure = makeShipBackwardsFigure();
-
-    shootWaitingToBeEmmited = false;
-
+    
     entities = [ this.entityPlayer ];
+    asteroidsDestroyedCounter = 0;
+    waveIndex = 0;
+
+    isPaused = false;
 }
