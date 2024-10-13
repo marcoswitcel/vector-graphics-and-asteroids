c53e5d7bfca236c1d97eda565daa9d3d3068e8a2
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-10-13 16:27:48 -0300

    .

--- src/entity.ts
+++ src/entity.ts
@@ -9,7 +9,7 @@ export const lineFigure = Symbol('lineFigure');
 export type EntityType = 'player' | 'shoot' | 'asteroids' | 'fragments' | 'entity';
 
 export const maxAngularVelocitySpaceShip = 7;
-export const angularAccelerationSpaceShip = maxAngularVelocitySpaceShip / 0.45; // @note leva meio segundo pra chegar no máximo
+export const angularAccelerationSpaceShip = maxAngularVelocitySpaceShip / 0.45; // @note leva 0.45 segundo(s) pra chegar no máximo
 
 /**
  * @note João, considerar criar um atributo para indicar que a entidade deve ser
